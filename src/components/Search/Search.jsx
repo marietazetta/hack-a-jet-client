8f8e9ba9965e476a169ca0f7fd645b9fb19bc995
Paloma Rehecho <paloma_re@hotmail.com>
2024-05-16 13:49:34 +0100

    logics finished

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,4 +1,4 @@
-import { Form, FloatingLabel, ListGroup } from 'react-bootstrap'
+import { Form, ListGroup } from 'react-bootstrap'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
@@ -8,10 +8,16 @@ const API_URL = import.meta.env.VITE_API_URL
 const Search = () => {
     const [companyQuery, setCompanyQuery] = useState('')
     const [filteredCompanies, setFilteredCompanies] = useState([])
+    const [option, setOption] = useState(false)
+
+    const handleOption = (boolean) => {
+        setOption(boolean)
+    }
 
     const handleCompanyQuery = event => {
         const { value: query } = event.target
         setCompanyQuery(query)
+        setOption(false)
         getFilteredCompanies(query)
     }
 
@@ -24,27 +30,24 @@ const Search = () => {
             .catch(err => console.log(err))
     }
 
+
+
     return (
         <div className='Search' >
-            <FloatingLabel
-                controlId="floatingInput"
-                label="search your operator"
-                className="mb-3"
-            >
-                <Form.Control
-                    type="text"
-                    placeholder="search your operator"
-                    value={companyQuery}
-                    onChange={handleCompanyQuery}
-                />
-            </FloatingLabel>
+
+            <Form.Control
+                type="text"
+                placeholder="search your operator"
+                value={companyQuery}
+                onChange={handleCompanyQuery}
+            />
             <ListGroup style={{ position: 'absolute', zIndex: 1000 }}>
 
                 {
                     filteredCompanies.map(company => {
                         return (
                             <ListGroup.Item key={company.id}>
-                                <Link to={`/operators/${company.id}`}>
+                                <Link to={`/operators/${company.id}`} onClick={handleOption}>
                                     {company.company}
                                 </Link>
                             </ListGroup.Item>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,38 +1,53 @@
 import { Container } from 'react-bootstrap'
+import './HomePage.css'
 
 
 const HomePage = () => {
 
     return (
 
-        <Container className="mt-5">
-            <h2 className='mb-5'>HACK A JET</h2>
+        <div>
 
-            <h3> Elevate Your Business with Hack a Jet </h3>
-            <p> Join the premier network of luxury jet providers.</p>
+            <video
+                src="https://res.cloudinary.com/dzncdwx7u/video/upload/v1715857418/1650628-hd_1920_1080_25fps_gfm0lm.mp4"
+                width='120%'
+                autoPlay
+            />
 
-            <p>At Hack a Jet, we are committed to connecting discerning travelers with the most exceptional private jet experiences. </p>
-            <p>Our exclusive network of partners shares our passion for providing unparalleled service and creating unforgettable journeys. </p>
 
-            <p> If your company is ready to elevate its offerings and cater to the most discerning clientele, we invite you to apply to join our esteemed network.</p>
-            <br />
-            <h4>Benefits of Partnership: </h4>
-            <ul>
-                <li>Access to a global network of affluent clientele</li>
-                <li>Exposure to a curated selection of luxury jet providers</li>
-                <li>Marketing and promotional support from Hack a Jet</li>
-                <li>Dedicated account manager to assist with your needs</li>
-            </ul>
-            <br />
-            <h4>Partnership Requirements:</h4>
+            <Container >
 
-            <ul>
-                <li>Proven track record of providing exceptional luxury jet services</li>
-                <li>Commitment to safety, reliability, and customer satisfaction</li>
-                <li>Fleet of well-maintained and luxurious private jets</li>
-                <li>Experienced and highly qualified crew</li>
-            </ul>
-        </Container>
+
+
+                <h2 className='mb-5'>HACK A JET</h2>
+
+                <h3> Elevate Your Business with Hack a Jet </h3>
+                <p> Join the premier network of luxury jet providers.</p>
+
+                <p>At Hack a Jet, we are committed to connecting discerning travelers with the most exceptional private jet experiences. </p>
+                <p>Our exclusive network of partners shares our passion for providing unparalleled service and creating unforgettable journeys. </p>
+
+                <p> If your company is ready to elevate its offerings and cater to the most discerning clientele, we invite you to apply to join our esteemed network.</p>
+                <br />
+                <h4>Benefits of Partnership: </h4>
+                <ul>
+                    <li>Access to a global network of affluent clientele</li>
+                    <li>Exposure to a curated selection of luxury jet providers</li>
+                    <li>Marketing and promotional support from Hack a Jet</li>
+                    <li>Dedicated account manager to assist with your needs</li>
+                </ul>
+                <br />
+                <h4>Partnership Requirements:</h4>
+
+                <ul>
+                    <li>Proven track record of providing exceptional luxury jet services</li>
+                    <li>Commitment to safety, reliability, and customer satisfaction</li>
+                    <li>Fleet of well-maintained and luxurious private jets</li>
+                    <li>Experienced and highly qualified crew</li>
+                </ul>
+            </Container>
+
+        </div>
 
     )
 }
