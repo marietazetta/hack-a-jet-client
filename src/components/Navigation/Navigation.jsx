6c1fdc2d16c85b91f6d74f619448e5c244d04d41
Maria Ramirez <maria.ramirez.laguia@gmail.com>
2024-05-16 14:50:09 +0200

    styles added

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -42,31 +42,7 @@ const Navigation = () => {
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
-                {/* <Navbar bg="dark" data-bs-theme="dark" variant="dark" expand="md" >
-                    <Container fluid>
 
-                        <Navbar.Brand as={Link} to="/">
-                            <img src="./src/assets/HackAJet.jpg" alt="HackAJet Logo" />
-                        </Navbar.Brand>
-                        <Link to="/">
-                            <Nav.Link href="/" as="span"><img src="./src\assets\HackAJet.jpg" alt="LOGO" /></Nav.Link>
-                        </Link> 
-
-                        <Nav className="me-auto">
-                            <Link to="/operators">
-                                <Nav.Link href="/operators" as="span">Operators</Nav.Link>
-                            </Link>
-
-                            <Link to="/aircrafts">
-                                <Nav.Link href="/aircrafts" as="span">Fleet</Nav.Link>
-                            </Link>
-                            <Link to="/membership">
-                                <Nav.Link href="/membership" as="span">Become a member</Nav.Link>
-                            </Link>
-                            <Search />
-                        </Nav>
-                    </Container>
-                </Navbar> */}
             </nav>
         </>
 
